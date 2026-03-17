cabc3200db8e5079ee39355f80b0bc3a47383ab1
victor-rrev <victor.reverdatto@gmail.com>
2026-03-17 19:22:21 +0000

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // add javascript here
+function showTip() {
+  document.getElementById("tip").innerText =
+    "Study in short sessions and take breaks!";
+}
+
+function greetUser() {
+  let name = document.getElementById("name").value;
+  document.getElementById("greeting").innerText =
+    "Hello, " + name + "!";
+}
